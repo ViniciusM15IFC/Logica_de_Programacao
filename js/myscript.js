60147c0d53a8e0653eccb7408aa6ef01b3a11e76
ViniciusM15IFC <142498596+ViniciusM15IFC@users.noreply.github.com>
2023-10-17 11:45:36 +0000

    aula 2

--- js/myscript.js
+++ js/myscript.js
@@ -1,10 +1,14 @@
 alert('Olá galera')
 function trocarFrase() {
-    var elemento = document.getElementById('frase');
+    var elemento = document.getElementById("frase");
     elemento.innerHTML = 'Funcionou, a frase mudou!';
     elemento.style.color = "red";
     elemento.style.fontSize = "25px";
     elemento.style.backgroundColor = "green";
     document.body.style.backgroundColor = "blue";
     elemento.innerHTML = "<img src='img/download.png'>"
+}
+function trocarClasse() {
+    var fraseAtual = document.getElementById("frase")
+    fraseAtual.className = "h1_2"
 }
