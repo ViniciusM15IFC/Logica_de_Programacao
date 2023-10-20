41a7454eacc598e99031555b03d2688a75345f68
ViniciusM15IFC <142498596+ViniciusM15IFC@users.noreply.github.com>
2023-10-20 18:06:33 +0000

    aula 3

--- js/myscript.js
+++ js/myscript.js
@@ -1,13 +1,3 @@
-
-function trocarFrase() {
-    var elemento = document.getElementById("frase");
-    elemento.innerHTML = 'Funcionou, a frase mudou!';
-    elemento.style.color = "red";
-    elemento.style.fontSize = "25px";
-    elemento.style.backgroundColor = "green";
-    document.body.style.backgroundColor = "blue";
-    elemento.innerHTML = "<img src='img/download.png'>"
-}
 function trocarClasse() {
     var fraseAtual = document.getElementById("frase")
     fraseAtual.className = "h1_2"
