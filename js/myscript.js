03d4bea0bd71319bc285e5597559241acb8e9ae6
ViniciusM15IFC <142498596+ViniciusM15IFC@users.noreply.github.com>
2023-11-10 17:49:27 +0000

    desisto

--- js/myscript.js
+++ js/myscript.js
@@ -9,12 +9,12 @@ function trocarTexto() {
 }
 function somar() {
     var n1 = document.getElementById("n1");
-    var n1 = parseInt(n1.value)
+    var n1 = parseInt(n1.value);
     var n2 = document.getElementById("n2");
-    var n2 = parseInt(n2.value)
-    var soma = n1 + n2
+    var n2 = parseInt(n2.value);
+    var soma = n1 + n2;
     var elemento = document.getElementById("resultado");
-    elemento.value = soma
+    elemento.value = soma;
 }
 function subtrair() {
     var n1 = document.getElementById("n1");
@@ -66,5 +66,37 @@ function login() {
         if (senha == 12345) {
             alert('certo')
         }
+    }
+}
+function verificar() {
+    var nome = document.getElementById("nome").value
+    var email = document.getElementById("email").value
+    var senha = document.getElementById("senha").value
+    var idade = document.getElementById("idade").value
+    var altura = document.getElementById("altura").value
+    var peso = document.getElementById("peso").value
+    if (nome != 'fulano') {
+        alert('Nome errado')
+        return
+    }
+    if (email != 'nada@nada.com') {
+        alert('Email errado')
+        return
+    }
+    if ((senha != '1234') && (senha != '4321')) {
+        alert('Senha errada')
+        return
+    }
+    if (idade < 10) {
+        alert('Idade errada')
+        return
+    }
+    if (altura <= 1.40) {
+        alert('Altura errada')
+        return
+    }
+    if ((peso > 40) && (peso > 150)) {
+        alert('Peso errado')
+        return
     }
 }
